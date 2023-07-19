2e7adcd0bc8e1438bd669ceaf06c21884422bd35
Daniel dos Santos Araujo <denenewton@icloud.com>
2023-07-19 19:25:58 -0300

    add crud update create delete search

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -1,82 +1,55 @@
-import { verifyIdField, hasErrosMovie } from "../../utils/utilities";
+import {
+  verifyIdField,
+  hasErrosMovie,
+  ifMovieExists,
+  payloadToSend,
+  verifyUniqueFields,
+} from "../../utils/hasErrors"
 
 export const Mutation = {
-
   createMovie: async (_, { data }, { Movie }) => {
     if (hasErrosMovie(data)) return hasErrosMovie(data);
 
-    const movie = await Movie.findOne({ id: Number(data.id) });
-
-    if (movie) {
-      return {
-        __typename: "Error",
-        message: "This ID already exists try another one!!",
-      };
-    }
+    if (await verifyUniqueFields(Movie, data))
+      return await verifyUniqueFields(Movie, data);
 
     const _movie = await Movie.create(data);
     if (_movie) return payloadToSend(_movie);
   },
 
-
   updateMovie: async (_, { data, id }, { Movie }) => {
     if (verifyIdField(id)) return verifyIdField(id);
 
-    const mov = await Movie.findOne({ id: id });
-
-    if (!mov) {
-      return {
-        __typename: "Error",
-        message: "Sorry! this movie doesn't exisist!!",
-      };
-    }
+    if(await ifMovieExists(Movie, id, data?.title)) 
+    return  await ifMovieExists(Movie, id, data?.title)
+    
 
     if (data?.id) {
       return {
         __typename: "Error",
-        message: "Sorry! id can't be updated!!",
+        message: "Sorry! id fields cannot be updated.",
       };
     }
 
-    var movie = await Movie.findOneAndUpdate({ id: Number(id) }, data, {new : true});
+    var movie = await Movie.findOneAndUpdate({ id: Number(id) }, data, {
+      new: true,
+    });
 
-    if (!movie){
-      return {
-        __typename: "Error",
-        message: "Sorry! this movie doesn't exisist!!",
-      };
-    }
-
-     return payloadToSend(movie);
-
-
+    return payloadToSend(movie);
   },
 
-  deleteMovie: async (_, { id }, { Movie }) =>{
+  deleteMovie: async (_, { id }, { Movie }) => {
     if (verifyIdField(id)) return verifyIdField(id);
 
-    const movie  = await Movie.findOneAndDelete({ id: Number(id)})
+    const movie = await Movie.findOneAndDelete({ id: Number(id) });
 
-    if (!movie){
+    if (!movie) {
       return {
         __typename: "Error",
         message: "Sorry! this movie doesn't exisist!!",
       };
     }
 
-    if(movie) return payloadToSend(movie)
-  }
-    
+    if (movie) return payloadToSend(movie);
+  },
 };
-
-function payloadToSend(_movie) {
-  delete _movie["_doc"]._id;
-  delete _movie["_doc"].__v;
-
-  var payload = _movie["_doc"];
-
-  return {
-    __typename: "Movie",
-    ...payload,
-  };
-}
--- src/utils/HasErrors.js
+++ src/utils/HasErrors.js
@@ -1,5 +1,29 @@
-export function hasErrorsMovie(data) {
+export function hasErrosMovie(data) {
+  if(verifyRequiredFields(data)) return verifyRequiredFields(data)
+
+  if (verifyIdField(data.id)) return verifyIdField(data.id);
+
+  if(verifyGenreField(data.genre)) return verifyGenreField(data.genre)
+
+  if (verifyYearField(data.year)) return verifyYearField(data.year)
+    
+  return false;
+}
+
+export function verifyYearField(year){
   var onlyNumbers = new RegExp("^[0-9]+$");
+  if (onlyNumbers.test(year)) {
+    const date = new Date();
+    if (year < 1985 || year > date.getFullYear()) {
+      return {
+        __typename: "Error",
+        message: "Year need to be [1985 - 2023].",
+      };
+    }
+  }
+}
+
+export  function verifyRequiredFields(data){
   if (
     !data.title ||
     !data.year ||
@@ -13,22 +37,102 @@ export function hasErrorsMovie(data) {
         "This field is required:  title, year, genre, director, description.",
     };
   }
-  if (!onlyNumbers.test(data.id)) {
+}
+
+export function verifyIdField(id) {
+  var onlyNumbers = new RegExp("^[0-9]+$");
+  if (!onlyNumbers.test(id)) {
     return {
       __typename: "Error",
       message:
         "This ID is invalid! it needs to contain only numbers [0 - 9] with no signal!!",
     };
   }
-
-  if (onlyNumbers.test(data.year)) {
-    const date = new Date();
-    if (data.year < 1985 || data.year > date.getFullYear()) {
-      return {
-        __typename: "Error",
-        message: "Year need to be [1985 - 2023].",
-      };
-    }
-  }
   return false;
 }
+
+export function verifyGenreField(genre) {
+  const genres = [
+    "Adventure",
+    "Comedy",
+    "Romance",
+    "Fantasy",
+    "Action",
+    "Thriller",
+    "Drama",
+    "Teen",
+    "Science_Fiction",
+  ];
+
+  if (!genres.find((g) => g === genre)) {
+    return {
+      __typename: "Error",
+      message:
+        "Genre just can be one of these: Adventure,  Comedy,  Romance, Fantasy,  Action,  Thriller, Drama, Teen, Science_Fiction",
+    };
+  }
+}
+
+export async function verifyUniqueFields(Movie, data) {
+  const id = await Movie.findOne({ id: data.id });
+  const title = await Movie.findOne({ title: data.title });
+
+  if (id && title) {
+    return {
+      __typename: "Error",
+      message: "This movie already exists try another one!!",
+    };
+  }
+
+  if (id) {
+    return {
+      __typename: "Error",
+      message: "This ID already exists try another one!!",
+    };
+  }
+  if (title) {
+    return {
+      __typename: "Error",
+      message: "This title already exists try another one!!",
+    };
+  }
+}
+
+export async function ifMovieExists(Movie, id, title) {
+  const mov = await Movie.findOne({ id: id });
+    const mov_title = await Movie.findOne({ title: title });
+
+    if (!mov) {
+      return {
+        __typename: "Error",
+        message: "Sorry! this movie doesn't exisist!!",
+      };
+    }
+
+    if (mov_title) {
+      if (Number(mov_title?.id) === Number(id)) {
+        return {
+          __typename: "Error",
+          message: "It's the same title you had before, try another one!!",
+        };
+      }
+      return {
+        __typename: "Error",
+        message: "This title aldeary exisist try another one!!",
+      };
+    }
+}
+
+export function payloadToSend(_movie) {
+  delete _movie["_doc"]._id;
+  delete _movie["_doc"].__v;
+
+  var payload = _movie["_doc"];
+
+  return {
+    __typename: "Movie",
+    ...payload,
+  };
+}
+
+
